624644f7048bca9a2386d37317d083ac3b15e84d
Rodrigo Mendoza <rodrigoshariff@gmail.com>
2016-05-07 17:26:00 -0400

    Add widget

---
+++ app/src/main/java/com/example/rmendoza/birderjourney_gce/widget/WidgetProvider.java
@@ -0,0 +1,43 @@
+package com.example.rmendoza.birderjourney_gce.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.RemoteViews;
+
+import com.example.rmendoza.birderjourney_gce.MainActivity;
+import com.example.rmendoza.birderjourney_gce.R;
+
+/**
+ * Created by rodrigoshariff on 5/7/2016.
+ */
+public class WidgetProvider  extends AppWidgetProvider {
+
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            views.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+
+
+}
